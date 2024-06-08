67cc21d9fd0617c4fe33a903ef6b859967f59ee2
Noah <noahkra@gmail.com>
2024-06-08 23:33:24 +0200

    Build the entire thing I guess?

---
+++ ClustertruckSpeedrunMod/MainWindow.xaml.cs
@@ -0,0 +1,316 @@
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System.Configuration;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace ClustertruckSpeedrunMod
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			EnableTruckColor.Checked += EnableTruckColor_Checked;
+			EnableTruckColor.Unchecked += EnableTruckColor_Unchecked;
+
+			EnableSpeedometer.Checked += EnableSpeedometer_Checked;
+			EnableSpeedometer.Unchecked += EnableSpeedometer_Unchecked;
+
+			UnlockFPS.Checked += UnlockFPS_Checked;
+			UnlockFPS.Unchecked += UnlockFPS_Unchecked;
+
+			TargetFPS.ValueChanged += TargetFPS_ValueChanged;
+
+			LoadSettings();
+
+			TargetFPSValue.Text = TargetFPS.Value.ToString("0");
+		}
+
+		int GetSpeedUnitInt()
+		{
+			if ((bool)mps.IsChecked) return 0;
+			if ((bool)kmph.IsChecked) return 1;
+			if ((bool)mph.IsChecked) return 2;
+			return -1;
+		}
+
+		void SetSpeedUnitInt()
+		{
+			switch (Properties.Settings.Default.SpeedUnit)
+			{
+				case 0:
+					mps.IsChecked = true;
+					break;
+				case 1:
+					kmph.IsChecked = true;
+					break;
+				case 2:
+					mph.IsChecked = true;
+					break;
+			}
+		}
+
+		void SaveSettings(object sender = null, RoutedEventArgs e = null)
+		{
+			Properties.Settings.Default.ClustertruckPath = FolderPath.Text;
+			Properties.Settings.Default.EnableSpeedometer = (bool)EnableSpeedometer.IsChecked;
+			Properties.Settings.Default.EnableTruckColor = (bool)EnableTruckColor.IsChecked;
+			Properties.Settings.Default.TruckColor = TruckColor.Text;
+			Properties.Settings.Default.UnlockFPS = (bool)UnlockFPS.IsChecked;
+			Properties.Settings.Default.TargetFPS = (int)TargetFPS.Value;
+			Properties.Settings.Default.EnableFPSCounter = (bool)EnableFPSCounter.IsChecked;
+			Properties.Settings.Default.DisableJump = (bool)DisableJump.IsChecked;
+			Properties.Settings.Default.InvertSprint = (bool)InvertSprint.IsChecked;
+			Properties.Settings.Default.EnableTimer = (bool)EnableTimer.IsChecked;
+			Properties.Settings.Default.SpeedUnit = GetSpeedUnitInt();
+			Properties.Settings.Default.Save();
+		}
+
+		void LoadSettings()
+		{
+			FolderPath.Text = Properties.Settings.Default.ClustertruckPath;
+			EnableSpeedometer.IsChecked = Properties.Settings.Default.EnableSpeedometer;
+			EnableTruckColor.IsChecked = Properties.Settings.Default.EnableTruckColor;
+			TruckColor.Text = Properties.Settings.Default.TruckColor;
+			UnlockFPS.IsChecked = Properties.Settings.Default.UnlockFPS;
+			TargetFPS.Value = Properties.Settings.Default.TargetFPS;
+			EnableFPSCounter.IsChecked = Properties.Settings.Default.EnableFPSCounter;
+			DisableJump.IsChecked = Properties.Settings.Default.DisableJump;
+			InvertSprint.IsChecked = Properties.Settings.Default.InvertSprint;
+			EnableTimer.IsChecked = Properties.Settings.Default.EnableTimer;
+			SetSpeedUnitInt();
+		}
+		private void EnableTruckColor_Checked(object sender, RoutedEventArgs e)
+		{
+			TruckColor.IsEnabled = true;
+		}
+
+		private void EnableTruckColor_Unchecked(object sender, RoutedEventArgs e)
+		{
+			TruckColor.IsEnabled = false;
+		}
+
+		private void EnableSpeedometer_Checked(object sender, RoutedEventArgs e)
+		{
+			mps.IsEnabled = true;
+			kmph.IsEnabled = true;
+			mph.IsEnabled = true;
+		}
+
+		private void EnableSpeedometer_Unchecked(object sender, RoutedEventArgs e)
+		{
+			mps.IsEnabled = false;
+			kmph.IsEnabled = false;
+			mph.IsEnabled = false;
+		}
+
+		private void UnlockFPS_Checked(object sender, RoutedEventArgs e)
+		{
+			TargetFPS.IsEnabled = true;
+		}
+
+		private void UnlockFPS_Unchecked(object sender, RoutedEventArgs e)
+		{
+			TargetFPS.IsEnabled = false;
+		}
+
+		private void TargetFPS_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			TargetFPSValue.Text = TargetFPS.Value.ToString("0");
+		}
+
+
+
+		private void PatchButton_Click(object sender, RoutedEventArgs e)
+		{
+			Progress(0, "Starting...");
+			try
+			{
+				Progress(5, "Setting up paths...");
+				string gameAssemblyPath = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\Assembly-CSharp.dll";
+				string gameAssemblyPathOriginal = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\Assembly-CSharp-ORIGINAL.dll";
+				string localPatchPath = $"{Environment.CurrentDirectory}\\ClustertruckSpeedrunModLib.dll";
+				string localHarmonyPath = $"{Environment.CurrentDirectory}\\0Harmony.dll";
+				string gamePatchPath = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\ClustertruckSpeedrunModLib.dll";
+				string gameHarmonyPath = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\0Harmony.dll";
+				string unityEnginePath = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\UnityEngine.dll";
+				string unityEngineUIPath = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\UnityEngine.UI.dll";
+				string localUnityEnginePath = $"{Environment.CurrentDirectory}\\UnityEngine.dll";
+				string localUnityEngineUIPath = $"{Environment.CurrentDirectory}\\UnityEngine.UI.dll";
+
+
+				Progress(10, "Backing up Assembly-CSharp.dll...");
+
+				// Wrong folder?
+				if (!File.Exists(gameAssemblyPath)) {
+					throw new Exception("Assembly-CSharp.dll not found.\n\nAre you sure you selected the right folder?");
+				}
+
+				// Necessary dlls?
+				if (!File.Exists(localPatchPath))
+				{
+					throw new Exception("ClustertruckSpeedrunModLib.dll not found.\n\nPlease make sure it is located in the execution directory.");
+				}
+				if (!File.Exists(localHarmonyPath))
+				{
+					throw new Exception("0Harmony.dll not found.\n\nPlease make sure it is located in the execution directory.");
+				}
+
+				// Do not judge me... I'm happy enough I got this working as is
+				File.Copy(unityEnginePath, localUnityEnginePath, true);
+				File.Copy(unityEngineUIPath, localUnityEngineUIPath, true);
+
+				// Create a backup of the original Assembly-CSharp.dll. Patching will be done from here.
+				if (!File.Exists(gameAssemblyPathOriginal))
+				{
+					File.Copy(gameAssemblyPath, gameAssemblyPathOriginal);
+				}
+
+				File.Copy(localPatchPath, gamePatchPath, true);
+				File.Copy(localHarmonyPath, gameHarmonyPath, true);
+
+				Progress(25, "Reading Assemblies...");
+
+				var gameAssembly = AssemblyDefinition.ReadAssembly(gameAssemblyPathOriginal);
+				var mainModule = gameAssembly.MainModule;
+
+				var patchAssembly = AssemblyDefinition.ReadAssembly(gamePatchPath);
+				mainModule.AssemblyReferences.Add(patchAssembly.Name);
+
+				Progress(35, "Setting up Entrypoint...");
+				TypeDefinition introHandlerType = mainModule.Types.FirstOrDefault(type => type.Name == "info");
+				MethodDefinition entryPoint = introHandlerType.Methods.FirstOrDefault(m => m.Name == "Awake");
+
+				// Ensure the entry point and instructions are not null
+				if (entryPoint != null && entryPoint.Body != null && entryPoint.Body.Instructions.Count > 0)
+				{
+					// Inject code to load and execute the Harmony patches
+					var ilProcessor = entryPoint.Body.GetILProcessor();
+					var firstInstruction = entryPoint.Body.Instructions[0];
+
+					Progress(40, "Injecting patches...");
+					var patchType = patchAssembly.MainModule.Types.FirstOrDefault(t => t.FullName == "ClustertruckSpeedrunModLib.Patcher");
+					var patchMethod = patchType.Methods.FirstOrDefault(m => m.Name == "DoPatching");
+
+					var color = System.Drawing.ColorTranslator.FromHtml((bool)EnableTruckColor.IsChecked ? TruckColor.Text : "#FFFFFF");
+
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create((bool)EnableSpeedometer.IsChecked ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0)); // EnableSpeedometer
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create(OpCodes.Ldc_I4, GetSpeedUnitInt())); // SpeedUnit
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create(OpCodes.Ldc_R4, (float)color.R / 255f)); // TruckColor.r
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create(OpCodes.Ldc_R4, (float)color.G / 255f)); // TruckColor.g
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create(OpCodes.Ldc_R4, (float)color.B / 255f)); // TruckColor.b
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create(OpCodes.Ldc_I4, (bool)UnlockFPS.IsChecked ? (int)TargetFPS.Value : 90)); // TargetFramerate
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create((bool)EnableFPSCounter.IsChecked ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0)); // EnableFPSCounter
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create((bool)DisableJump.IsChecked ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0)); // DisableJump
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create((bool)InvertSprint.IsChecked ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0)); // InvertSprint
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create((bool)EnableTimer.IsChecked ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0)); // EnableTimer
+
+					ilProcessor.InsertBefore(firstInstruction, ilProcessor.Create(OpCodes.Call, mainModule.ImportReference(patchMethod)));
+
+
+					Progress(95, "Writing modified assembly to disk...");
+					gameAssembly.Write(gameAssemblyPath);
+
+					Progress(100, "Assembly patched successfully! :D");
+				} 
+				else
+				{
+					MessageBox.Show("Error: Unable to find suitable injection point.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				}
+			} 
+			catch (Exception ex)
+			{
+				if (ex.Message.Contains("user-mapped section open"))
+				{
+					MessageBox.Show($"Please close the game before trying to apply the patches!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				}
+				else if(ex.Message.Contains("Parameter 'htmlColor'"))
+				{
+					MessageBox.Show($"Truck colour \"{TruckColor.Text}\" is not a valid colour.\n\nPlease use HTML HEX formatting (ex. #FF00FF). ", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				}
+				else
+				{
+					MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				}
+
+				Progress(0, "Patching interrupted :(");
+			}
+		}
+
+		private void BrowseFolders_Click(object sender, RoutedEventArgs e)
+		{
+			var dialog = new CommonOpenFileDialog();
+			dialog.IsFolderPicker = true;
+
+			if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+			{
+				FolderPath.Text = dialog.FileName;
+			}
+		}
+
+		void Progress(int val,  string msg)
+		{
+			ProgressVal.Value = val;
+			ProgressText.Text = msg;
+		}
+
+		private void UnpatchButton_Click(object sender, RoutedEventArgs e)
+		{
+			Progress(0, "Starting...");
+			try
+			{
+				Progress(5, "Setting up paths...");
+				string gameAssemblyPath = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\Assembly-CSharp.dll";
+				string gameAssemblyPathOriginal = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\Assembly-CSharp-ORIGINAL.dll";
+				string gamePatchAssemblyPath = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\ClustertruckSpeedrunModLib.dll";
+				string gameHarmonyPath = $"{FolderPath.Text}\\Clustertruck_Data\\Managed\\0Harmony.dll";
+
+				Progress(40, "Restoring Assembly-CSharp.dll...");
+				if (File.Exists(gameAssemblyPathOriginal))
+				{
+					File.Copy(gameAssemblyPathOriginal, gameAssemblyPath, true);
+					File.Delete(gameAssemblyPathOriginal);
+				}
+
+				Progress(80, "Removing libraries...");
+				if (File.Exists(gamePatchAssemblyPath))
+				{
+					File.Delete(gamePatchAssemblyPath);
+				}
+				if (File.Exists(gameHarmonyPath))
+				{
+					File.Delete(gameHarmonyPath);
+				}
+
+				Progress(100, "Game files restored!");
+
+			} 
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+
+		}
+
+		protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+		{
+			SaveSettings();
+			base.OnClosing(e);
+		}
+	}
+}
---
+++ ClustertruckSpeedrunModLib/ClustertruckSpeedrunModLib.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ClustertruckSpeedrunModLib
+{
+	public static class Patcher
+	{
+		public static Rigidbody playRig;
+		public static int FPSinterval;
+		public static string prevFPS;
+		public static float avgFPS;
+
+		// Preferences
+		public static bool EnableSpeedometer;
+		public static int SpeedUnit;
+		public static Color TruckColor;
+		public static int TargetFramerate;
+		public static bool EnableFPSCounter;
+		public static bool DisableJump;
+		public static bool InvertSprint;
+		public static bool EnableTimer;
+
+		public static void DoPatching(bool _enableSpeedometer, int _speedUnit, float _truckColorR, float _truckColorG, float _truckColorB, int _targetFramerate, bool _enableFPSCounter, bool _disableJump, bool _invertSprint, bool _enableTimer)
+		{
+			FPSinterval = 0;
+
+			EnableSpeedometer = _enableSpeedometer;
+			SpeedUnit = _speedUnit;
+			TruckColor = new Color(_truckColorR, _truckColorG, _truckColorB, 1f);
+			TargetFramerate = _targetFramerate;
+			EnableFPSCounter = _enableFPSCounter;
+			DisableJump = _disableJump;
+			InvertSprint = _invertSprint;
+			EnableTimer = _enableTimer;
+
+			try
+			{
+				var harmony = new Harmony("com.clustertruckspeedrun.mod");
+
+				Console.WriteLine("[SPEEDRUNMOD] Applying MenuTitlePatch...");
+				MenuTitlePatch.Apply(harmony);
+
+				Console.WriteLine("[SPEEDRUNMOD] Applying TimerPatch...");
+				AssignPlayer.Apply(harmony);
+				TimerPatch.Apply(harmony);
+
+				Console.WriteLine("[SPEEDRUNMOD] Applying TruckColorPatch...");
+				TruckColorPatch.Apply(harmony);
+		
+				Console.WriteLine("[SPEEDRUNMOD] Applying FPSPatch...");
+				FPSPatch.Apply(harmony);
+			
+				if (DisableJump)
+				{
+					Console.WriteLine("[SPEEDRUNMOD] Applying JumplessPatch...");
+					JumplessPatch.Apply(harmony);
+				}
+				if (InvertSprint)
+				{
+					Console.WriteLine("[SPEEDRUNMOD] Applying SprintPatch...");
+					SprintPatch.Apply(harmony);
+				}
+				if (EnableTimer)
+				{
+					Console.WriteLine("[SPEEDRUNMOD] Applying ShowTimerPatch...");
+					ShowTimerPatch.Apply(harmony);
+				}
+
+				Console.WriteLine("[SPEEDRUNMOD] All patches applied successfully!");
+			} 
+			catch (Exception ex)
+			{
+				Console.WriteLine($"[SPEEDRUNMOD] Patching Failed: {ex}");
+			}
+		}
+	}
+	
+	static class MenuTitlePatch
+	{
+		public static void Apply(Harmony harmony)
+		{
+			var original = typeof(menuBar).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			var patch = typeof(MenuTitlePatch).GetMethod(nameof(Postfix));
+
+			harmony.Patch(original, postfix: new HarmonyMethod(patch));
+		}
+
+		public static void Postfix(menuBar __instance)
+		{
+			Transform parent = __instance.leftMask.Find("Main"); 
+			Text playButtonText = __instance.leftMask.Find("Main").Find("play").Find("Text").GetComponent<Text>();
+			GameObject title = new GameObject("SpeedrunmodTitle");
+			Text titleText = title.AddComponent<Text>();
+			titleText.text = "Clustertruck Speedrun Mod v1.0.0";
+			titleText.font = playButtonText.font;
+			titleText.color = playButtonText.color;
+			titleText.fontSize = 32;
+			titleText.alignment = TextAnchor.MiddleCenter;
+			RectTransform titleRT = title.GetComponent<RectTransform>();
+			titleRT.SetParent(parent);
+			titleRT.sizeDelta = new Vector2(1000f, 40f);
+			titleRT.localScale = new Vector3(0.661255f, 0.661255f, 0.661255f);
+			titleRT.localPosition = new Vector3(0f, 270f, 0f);
+		}
+	}
+
+	static class AssignPlayer // Required for speedometer
+	{
+		public static void Apply(Harmony harmony)
+		{
+			var original = typeof(player).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			var patch = typeof(AssignPlayer).GetMethod(nameof(Postfix));
+
+			harmony.Patch(original, postfix: new HarmonyMethod(patch));
+		}
+
+		public static void Postfix(player __instance)
+		{
+			Patcher.playRig = __instance.rig;
+		}
+	}
+
+	static class TimerPatch
+	{
+		public static void Apply(Harmony harmony)
+		{
+			var original = typeof(PlayerClock).GetMethod(nameof(PlayerClock.SetTimeText));
+
+			var patch = typeof(TimerPatch).GetMethod(nameof(Postfix));
+
+			harmony.Patch(original, postfix: new HarmonyMethod(patch));
+		}
+
+		public static void Postfix(PlayerClock __instance, ref Text ____TimeText, ref Text ____NameText, string val)
+		{
+			____TimeText.alignment = TextAnchor.LowerRight;
+			Patcher.avgFPS += Time.unscaledDeltaTime;
+			if (Patcher.FPSinterval == 0)
+			{
+				Patcher.prevFPS = (1 / (Patcher.avgFPS / 50)).ToString("0");
+				Patcher.avgFPS = 0;
+			}
+			Patcher.FPSinterval += 1;
+			if (Patcher.FPSinterval == 50)
+			{
+				Patcher.FPSinterval = 0;
+			}
+
+			string velocity;
+
+			switch (Patcher.SpeedUnit)
+			{
+				case 1:
+					velocity = $"{(Patcher.playRig.velocity.magnitude * 3.6f).ToString("0")}km/h";
+					break;
+				case 2:
+					velocity = $"{(Patcher.playRig.velocity.magnitude * 2.236936f).ToString("0")}mph";
+					break;
+				default:
+					velocity = $"{Patcher.playRig.velocity.magnitude.ToString("0")}m/s";
+					break;
+			}
+
+			____NameText.text = String.Empty;
+			____TimeText.text = $"{ (Patcher.DisableJump ? "Jumpless\n\n" : "") }{ (Patcher.EnableSpeedometer ? $"{velocity}\n\n" : "") }{ (Patcher.EnableFPSCounter ? $"{Patcher.prevFPS}fps\n" : "") }{ val }";
+		}
+	}
+
+	static class TruckColorPatch
+	{
+		public static void Apply(Harmony harmony)
+		{
+			var original = typeof(LandfallTwitch).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			var patch = typeof(TruckColorPatch).GetMethod(nameof(Postfix));
+
+			harmony.Patch(original, postfix: new HarmonyMethod(patch));
+		}
+		
+		public static void Postfix(ref Material ___mTruckMaterial)
+		{
+			___mTruckMaterial.color = Patcher.TruckColor;
+		}
+	}
+
+	static class FPSPatch
+	{
+		public static void Apply(Harmony harmony)
+		{
+			var original = typeof(Manager).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			var patch = typeof(FPSPatch).GetMethod(nameof(Postfix));
+
+			harmony.Patch(original, postfix: new HarmonyMethod(patch));
+		}
+
+		public static void Postfix()
+		{
+			Application.targetFrameRate = Patcher.TargetFramerate;
+		}
+	}
+
+	static class JumplessPatch
+	{
+		public static void Apply(Harmony harmony)
+		{
+			var origami = typeof(player).GetMethod("JumpRayHit", BindingFlags.NonPublic | BindingFlags.Instance);
+			var orange  = typeof(player).GetMethod("Update", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			var origamiPatch = typeof(JumplessPatch).GetMethod(nameof(Prefix));
+			var orangePatch  = typeof(JumplessPatch).GetMethod(nameof(Transpiler));
+
+			harmony.Patch(origami, prefix: new HarmonyMethod(origamiPatch));
+			harmony.Patch(orange, transpiler: new HarmonyMethod(orangePatch));
+		}
+
+		public static void Prefix(ref bool ___canForward)
+		{
+			___canForward = false;
+			return;
+		}
+
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var codes = new List<CodeInstruction>(instructions);
+			for (var i = 0; i < codes.Count; i++)
+			{
+				if (codes[i].opcode == OpCodes.Ldstr && (string)codes[i].operand == "Jump")
+				{
+					codes[i].operand = "Unbound";
+				}
+			}
+
+			return codes.AsEnumerable();
+		}
+	}
+
+	static class SprintPatch
+	{
+		public static void Apply(Harmony harmony)
+		{
+			var original = typeof(player).GetMethod("Update", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			var patch = typeof(SprintPatch).GetMethod(nameof(Transpiler));
+
+			harmony.Patch(original, transpiler: new HarmonyMethod(patch));
+		}
+
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var codes = new List<CodeInstruction>(instructions);
+			for (var i = 0; i < codes.Count; i++)
+			{
+				if (codes[i].opcode == OpCodes.Ldstr && (string)codes[i].operand == "Sprint")
+				{
+					codes[i + 2].opcode = OpCodes.Brfalse_S;
+					break;
+				}
+			}
+
+			return codes.AsEnumerable();
+		}
+	}
+
+	static class ShowTimerPatch
+	{
+		public static void Apply(Harmony harmony)
+		{
+			var original = typeof(GameManager).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			var patch = typeof(ShowTimerPatch).GetMethod(nameof(Postfix));
+
+			harmony.Patch(original, postfix: new HarmonyMethod(patch));
+		}
+
+		public static void Postfix(ref PlayerClock ___PlayerClock)
+		{
+			info.ShowClock = true;
+		}
+	}
+}
