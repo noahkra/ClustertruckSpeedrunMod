99bd76789ae01e55db30c6e8dc6abab342357691
Noah <noahkra@gmail.com>
2024-06-09 19:15:07 +0200

    Remove unused references

--- ClustertruckSpeedrunMod/MainWindow.xaml.cs
+++ ClustertruckSpeedrunMod/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System.Reflection;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using System.IO;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
-using System.Configuration;
 using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace ClustertruckSpeedrunMod
--- ClustertruckSpeedrunModLib/ClustertruckSpeedrunModLib.cs
+++ ClustertruckSpeedrunModLib/ClustertruckSpeedrunModLib.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Runtime.InteropServices;
 using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
